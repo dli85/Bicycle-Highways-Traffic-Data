6933f14b9276242dc34f95ca261b94216283aa1d
David Li <dali7758769@gmail.com>
2022-11-30 02:09:05 -0500

    Add downloading refreshing

--- scerisecm_scraper.py
+++ scerisecm_scraper.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -10,6 +12,7 @@ import shutil
 from csvReader import read_csv
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 url = 'https://scerisecm.boston.gov/ScerIS/CmPublic/#/SearchCriteria?f=56'
 csv_path = 'search results.csv'
@@ -20,10 +23,12 @@ download_directory_path = project_path + '\\downloads'
 temp_directory_path_relative = './temp'
 download_directory_path_relative = './downloads'
 
-file_id = 1
+file_id = 980
 
 currentRow = 0
 search_list = []
+
+counter = 0
 
 
 def create_list(starting):
@@ -32,6 +37,7 @@ def create_list(starting):
 
 
 def crawl():
+    global counter
     global search_list
     global currentRow
     global file_id
@@ -81,7 +87,15 @@ def crawl():
     time.sleep(3)
     try:
         for i in range(currentRow, len(search_list)):
-            print("Progress: working on", str(currentRow + 1), "/", str(len(search_list)))
+            print("Progress: working on", str(currentRow + 1), "/", str(len(search_list)) + " " + search_list[i])
+
+            if counter >= 100:
+                counter = 0
+                print("Restarting, nothing to worry about...")
+                driver.close()
+                time.sleep(10)
+                crawl()
+                return
 
             string = search_list[i]
             # original_file field
@@ -95,13 +109,13 @@ def crawl():
             original_file_field.send_keys(string)
 
             # click archive_date_sort twice
-            WebDriverWait(driver, 10) \
+            WebDriverWait(driver, timeout=3) \
                 .until(EC.element_to_be_clickable((By.XPATH,
                                                    "/html/body/div[1]/div/div/div[2]/div[3]/div/div/div[2]/div["
                                                    "1]/div[2]/div[2]/div[2]/div/div[2]/div["
                                                    "2]/div/div[1]/div[ "
                                                    "1]/div/div/div/div/div/div[1]/div[1]/div[1]"))).click()
-            WebDriverWait(driver, 10) \
+            WebDriverWait(driver, timeout=3) \
                 .until(EC.element_to_be_clickable((By.XPATH,
                                                    "/html/body/div[1]/div/div/div[2]/div[3]/div/div/div[2]/div["
                                                    "1]/div[2]/div[2]/div[2]/div/div[2]/div["
@@ -114,7 +128,7 @@ def crawl():
 
             # select row and click
 
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
+            WebDriverWait(driver, timeout=3).until(EC.element_to_be_clickable((By.XPATH,
                                                                         "/html/body/div[1]/div/div/div["
                                                                         "2]/div[3]/div/div/div[2]/div["
                                                                         "1]/div[2]/div[ "
@@ -122,30 +136,30 @@ def crawl():
                                                                         "2]/div/div/div/div[1]/div"))).click()
 
             # Select and click view document button
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
+            WebDriverWait(driver, timeout=3).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
                                                                                   "2]/div[3]/div/div/div[2]/div["
                                                                                   "1]/div[2]/div[ "
                                                                                   "2]/div[2]/div/div[1]/nav/button[5]"))
                                             ).click()
             # select and click download
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
+            WebDriverWait(driver, timeout=3).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
                                                                                   "2]/div[3]/div/div[3]/div[1]/div["
                                                                                   "2]/div/div[ "
                                                                                   "4]/div[2]/div[1]/div[2]/div["
                                                                                   "1]/button[3]"))).click()
             time.sleep(1)
             # select and hit download left side
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
+            WebDriverWait(driver, timeout=3).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div["
                                                                                   "2]/div[3]/div/div[3]/div[1]/div["
                                                                                   "2]/div/div[ "
                                                                                   "4]/div[5]/div["
                                                                                   "7]/div[2]/button"))).click()
-
             # select and hit save
+            time.sleep(1)
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
                 (By.XPATH, "/html/body/div[1]/div/div/div[2]/div[3]/div/div[3]/div[1]/div[2]/div/div["
                            "4]/div[23]/div/div/div[2]/button[2]"))).click()
-            time.sleep(1)
+            time.sleep(1.5)
             for x in os.walk(temp_directory_path):
                 download_file_path = x[0] + '\\' + x[2][0]
                 filetype = x[2][0].split('.')[len(x[2][0].split('.')) - 1]
@@ -155,35 +169,21 @@ def crawl():
                            "." + filetype
                 # print(new_path)
                 Path(download_directory_path).mkdir(parents=True, exist_ok=True)
-                time.sleep(0.2)
+                time.sleep(0.5)
                 shutil.copy2(download_file_path, new_path)
-                time.sleep(0.2)
+                time.sleep(0.5)
                 os.remove(download_file_path)
                 os.rmdir(temp_directory_path)
                 break
 
-            time.sleep(1)
-
-            # files = glob.iglob('.\\downloads\\*')
-            # max_file = max(files, key=os.path.getctime)
-            # original_file_name = max_file
-            # max_file_split = max_file.split('\\')
-            # filename = max_file_split[len(max_file_split) - 1].split('.')
-            # extension = filename[len(filename) - 1]
-            # # print(max_file_split)
-            # # print(filename)
-            # # print(extension)
-            # new_file_name = "Type " + study_type + " " + str(currentRow) + "." + extension
-            # # print(new_file_name)
-            # os.rename(original_file_name, '.\\downloads\\' + new_file_name)
-
-            # hit back button
-            driver.find_element(By.XPATH, '//*[@id="btnBackToSearchResult"]').click()
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="btnBackToSearchResult"]')))\
+                .click()
             currentRow += 1
             file_id += 1
+            counter += 1
 
             time.sleep(0.5)
-    except ValueError:
+    except TimeoutException:
         driver.close()
         print("restarting... (nothing to worry about)")
         time.sleep(10)
